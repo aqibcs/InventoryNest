b6a4ed0036ec6818fd30c176698f6d1b67f202b6
aqibcs <csaqibshah@gmail.com>
2024-12-17 06:08:55 +0000

    Add UserProfile model to extend the default User model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='userprofile')
+    address = models.TextField(blank=True, null=True)
+    payment_info = models.JSONField(blank=True, null=True)
+    preferences = models.JSONField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
