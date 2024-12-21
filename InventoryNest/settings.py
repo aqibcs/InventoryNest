79736f8e3bcc11963afe2cbeb7fb538897f8a568
aqibcs <csaqibshah@gmail.com>
2024-12-21 09:51:18 +0000

    Update settings.py to include necessary configurations for the Cart app

--- InventoryNest/settings.py
+++ InventoryNest/settings.py
@@ -30,6 +30,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# Add CORS support
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
+
+CORS_ALLOW_CREDENTIALS = True
 
 # Application definition
 
@@ -47,11 +54,13 @@ INSTALLED_APPS = [
     'products',
     'orders',
     'cart',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -94,7 +103,6 @@ DATABASES = {
         'PORT': os.getenv('PG_PORT', '5432'),
     }
 }
-
 
 
 REST_FRAMEWORK = {
